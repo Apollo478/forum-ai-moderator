5216f969b1207346c04f0d534366249d99a4ec6a
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2025-04-20 23:41:21 +0100

    FEAT: sentiment analysis

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from transformers import pipelines
 app = Flask(__name__)
 model = Detoxify("original")
 summarizer = pipelines.pipeline("summarization", model="facebook/bart-large-cnn")
+sentiment = pipelines.pipeline("sentiment-analysis",model="cardiffnlp/twitter-roberta-base-sentiment")
 @app.route("/analyze",methods=["POST"])
 def analyze():
     data=request.get_json();
@@ -18,6 +19,15 @@ def summarize():
     text = data.get("text","")
     results = summarizer(text,max_length=280,min_length=30,do_sample=False)
     return jsonify({"summary":results[0]['summary_text']})
+
+
+@app.route("/sentiment",methods=["POST"])
+def sentiment_analysis():
+    data = request.get_json()
+    text = data.get("text")
+    result = sentiment(text)
+    return jsonify(result[0])   
+
 
 if __name__ == "__main__":
     app.run(port=5000)
