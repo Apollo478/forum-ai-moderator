6f1abb83ea7185413ba3540a8a343e201761de5e
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2025-04-20 23:20:49 +0100

    FEAT: summarization

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from detoxify import Detoxify
 from flask import Flask,request,jsonify
+from transformers import pipelines
 app = Flask(__name__)
 model = Detoxify("original")
+summarizer = pipelines.pipeline("summarization", model="facebook/bart-large-cnn")
 @app.route("/analyze",methods=["POST"])
 def analyze():
     data=request.get_json();
@@ -9,5 +11,13 @@ def analyze():
     results = model.predict(text)
     results = {key: float(value) for key, value in results.items()}
     return jsonify(results)
+
+@app.route("/summarize",methods=["POST"])
+def summarize():
+    data = request.get_json()
+    text = data.get("text","")
+    results = summarizer(text,max_length=280,min_length=30,do_sample=False)
+    return jsonify({"summary":results[0]['summary_text']})
+
 if __name__ == "__main__":
     app.run(port=5000)
